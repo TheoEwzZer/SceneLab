003c7fd4effb28eee860c1afcd5ef48ca401ae30
Theo Fabiano <theo.fabianomattei@gmail.com>
2025-09-19 13:45:44 -0400

    refactor: remove Apple-specific OpenGL hint

--- src/main.cpp
+++ src/main.cpp
@@ -35,9 +35,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-#endif
 
     GLFWwindow *window = glfwCreateWindow(kInitialWindowWidth,
         kInitialWindowHeight, "scenelab", nullptr, nullptr);
